72e42a36043fa36f7964abec883a10d5cc0732a2
FlorianFaneuil <florian.faneuil@gmail.com>
2023-12-26 09:07:15 +0100

    création de 20 tables qu'il faudra assigner aux différents restaurants

--- SQL/GenerationDonneesDeTravail.sql
+++ SQL/GenerationDonneesDeTravail.sql
@@ -32,3 +32,72 @@ VALUES (9, 'ter', 'Chemin', 'Paul Valéry', '80001', 'Amiens', '2eme étage');
 INSERT INTO adresses (numéro, complement, type_voie, nom, code_postal, ville, mention)
 VALUES (10, 'bis', 'Impasse', 'Emile Zola', '06001', 'Nice', '1er étage');
 
+
+DELETE FROM superadmins;
+
+INSERT INTO superadmins (nom, prenom, email, password)
+VALUES ('Super','Admin','super.admin@example.com','password');
+
+
+DELETE FROM tables;
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (1, 2, 'PRESENT');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (2, 4, 'PRESENT');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (3, 4, 'PRESENT');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (4, 6, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (5, 6, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (6, 8, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (7, 2, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (8, 3, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (9, 4, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (10, 6, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (11, 6, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (12, 8, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (13, 2, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (14, 4, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (15, 4, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (16, 6, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (17, 6, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (18, 8, 'LIBRE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (19, 2, 'RESERVEE');
+
+INSERT INTO tables (numero, nombre_place, etat)
+VALUES (20, 4, 'LIBRE');
+
